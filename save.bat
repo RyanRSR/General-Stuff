ee8d81a84d9241038c3e8c86155197f5ba51d288
RyanRSR <ryansic157@gmail.com>
2023-05-30 21:11:24 -0300

    ok

---
+++ save.bat
@@ -0,0 +1,10 @@
+git config --global user.email "ryansic157@gmail.com"
+git config --global user.name "RyanRSR"
+git clone https://github.com/RyanRSR/General-Stuff.git
+cd General-Stuff
+
+git add --all
+git commit -m "ok"
+git pull
+git fetch
+git push
